2a98dddfd86170b87e61d68628e1ff14333e4c32
Hesam <hesam.rastegari@gmail.com>
2022-05-06 16:54:43 +0430

    Basic and simple structure - 6th May

--- app/src/main/java/hessam/rastegari/weroom/MainActivity.java
+++ app/src/main/java/hessam/rastegari/weroom/MainActivity.java
@@ -1,14 +1,43 @@
 package hessam.rastegari.weroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+    ArrayList<String> dataSource;
+    LinearLayoutManager linearLayoutManager;
+    MyRvAdapter myRvAdapter;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.horizontalRv);
+        dataSource = new ArrayList<>();
+        dataSource.add("Hello");
+        dataSource.add("World");
+        dataSource.add("to");
+        dataSource.add("the");
+        dataSource.add("Code");
+        dataSource.add("City");
+        dataSource.add("*****");
+
+
+        linearLayoutManager = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        myRvAdapter = new MyRvAdapter(dataSource,MainActivity.this);
+        rv.setLayoutManager(linearLayoutManager);
+        rv.setAdapter(myRvAdapter);
     }
 }
