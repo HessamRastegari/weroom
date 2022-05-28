34c93780692799a46b4888de77c99402111e00a4
Hesam <hesam.rastegari@gmail.com>
2022-05-28 05:44:06 +0430

    Basic and simple structure - 28th May Sign in layout + google sign in

--- app/src/main/java/hessam/rastegari/weroom/RegisterActivity.java
+++ app/src/main/java/hessam/rastegari/weroom/RegisterActivity.java
@@ -12,8 +12,12 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -21,6 +25,12 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
@@ -29,18 +39,22 @@ import com.google.gson.GsonBuilder;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RetrofirServices retrofirServices;
     private ActivityRegisterBinding activityRegisterBinding;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_register);
 
+
+
         activityRegisterBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(activityRegisterBinding.getRoot());
+//        activityRegisterBinding.signinAnimationView.setSpeed(0.5f);
 
 //        Gson gson = new GsonBuilder()
 //                .setLenient()
@@ -52,6 +66,19 @@ public class RegisterActivity extends AppCompatActivity {
                 .build();
 
         retrofirServices = retrofit.create(RetrofirServices.class);
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        activityRegisterBinding.signInButton.setOnClickListener(this);
+
+
 
 
         activityRegisterBinding.btnSend.setOnClickListener(new View.OnClickListener() {
@@ -196,4 +223,61 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    protected void onStart() {
+
+//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//        if (account !=null) {
+//            Toast.makeText(this, "Name: "+account.getGivenName()+" \n Family Name: "+account.getFamilyName()+"\n Email: "+account.getEmail()+ "given name: "+ account.getGivenName()+"\n DisplayName: "+account.getDisplayName() , Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(RegisterActivity.this, MainFragmentActivity.class);
+//            startActivity(intent);
+//        }
+        super.onStart();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_button:
+                GoogleSignIn();
+
+                break;
+        }
+    }
+    private void GoogleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 122);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 122) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            Toast.makeText(this, "Name: "+account.getGivenName()+"Family Name: "+account.getFamilyName()+" Email: "+account.getEmail(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(RegisterActivity.this, MainFragmentActivity.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("GoogleSignIn", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this, "we are sorry, there is a problem with your sign in", Toast.LENGTH_SHORT).show();
+            //updateUI(null);
+        }
+    }
 }
